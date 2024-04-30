8fe2fac6c65bf68222ccae591a76953ee5151d1b
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-04-30 17:41:11 +0600

    basic project structure setup

---
+++ src/app/(dashboard)/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <h2 className='font-semibold text-2xl'>Project Pulse - Streamlining Projects, Simplifying Success</h2>
+        </div>
+    );
+};
+
+export default page;
---
+++ src/app/layout.jsx
@@ -0,0 +1,17 @@
+import { Public_Sans } from "next/font/google";
+import "./globals.css";
+
+const public_sans = Public_Sans({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Project Pulse - Streamlining Projects, Simplifying Success",
+  description: "A streamlined platform for managing projects and tasks, designed to simplify success.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={public_sans.className}>{children}</body>
+    </html>
+  );
+}
