3f35f1fee95acf94ef51b7b248cfe5b2c2d88f29
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-05-01 18:19:48 +0600

    fix: route link for project details

---
+++ src/app/(dashboard)/projects/project-details/[id]/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const page = ({ params }) => {
+    console.log(params.id)
+    return (
+        <div>
+            page
+        </div>
+    );
+};
+
+export default page;
--- src/components/projectsPage/ProjectCard/ProjectCard.jsx
+++ src/components/projectsPage/ProjectCard/ProjectCard.jsx
@@ -1,13 +1,12 @@
 import Link from 'next/link';
 import React from 'react';
-import { MdEdit, MdDelete } from "react-icons/md";
 import DeleteProjectModal from '../DeleteProjectModal/DeleteProjectModal';
 import UpdateProjectModal from '../UpdateProjectModal/UpdateProjectModal';
 
 const ProjectCard = ({ project }) => {
     return (
         <div className='relative overflow-hidden group'>
-            <Link href={`/projects/${project._id}`}>
+            <Link href={`/projects/project-details/${project._id}`}>
                 <div className='bg-white px-4 py-6 rounded flex flex-col  min-h-44 relative group overflow-hidden duration-300'>
                     <h1 className='text-xl font-semibold uppercase mb-3'>{project.project_name}</h1>
                     <p className='flex-1'>{project.project_description}</p>
