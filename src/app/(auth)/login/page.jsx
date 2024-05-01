cf6bbd0e8777f0d4203bfe6ffc119cf199b7ff07
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-05-01 12:23:18 +0600

    fix: responsiveness on login and register page

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 const LoginPage = () => {
 
     return (
-        <div className='max-w-[450px] py-8 rounded-md shadow bg-white px-10'>
+        <div className='max-w-[450px] py-8 rounded-md shadow bg-white px-3 md:px-10'>
             <LoginForm />
             <SocialLogin />
             <p className='text-center my-6'>Don't have an account ? <Link className='font-medium underline' href={'/register'}>SignUp</Link></p>
--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 
 const RegisterPage = () => {
     return (
-        <div className='max-w-[450px] py-8 rounded-md shadow bg-white px-10'>
+        <div className='max-w-[450px] py-8 rounded-md shadow bg-white px-3 md:px-10'>
             <RegisterForm />
             <SocialLogin />
             <p className='text-center my-6'>Already have an account ? <Link className='font-medium underline' href={'/login'}>Login</Link></p>
